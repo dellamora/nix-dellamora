955102b89342bf7ca11f3c0227af55d482ed9bd1
Francielle Dellamora <francielle@dellamora.dev>
2024-09-02 20:47:08 -0300

    add nix configuration

---
+++ flake.nix
@@ -0,0 +1,44 @@
+{
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs";
+  };
+  outputs = { self, nixpkgs }: {
+    packages."aarch64-darwin".default = 
+    let 
+      pkgs = nixpkgs.legacyPackages."aarch64-darwin";
+    in
+    pkgs.buildEnv {
+      name = "home-packages";
+      paths = with pkgs; [
+        # Shell and Version Control
+        git
+        gh
+        tmux
+
+        # Development Environment
+        devenv
+        neovim
+        ripgrep
+
+        # Programming Languages and Runtimes
+        ## JavaScript/TypeScript
+        nodejs
+        bun
+        ## Go
+        go
+        ## Haskell
+        ghc
+        cabal-install
+        stack
+        haskell-language-server
+        ## Agda
+        agda
+        (agda.withPackages (p: [ p.standard-library ]))
+
+        # macOS Window Management
+        yabai
+        skhd
+      ];
+    };
+  };
+}
